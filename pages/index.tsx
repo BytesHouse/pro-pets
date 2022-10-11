9376247196b5452be18ef5efbf597e509b39facc
Andrei Lihtgolt <a.lihtgolt@qorporates.com>
2022-10-11 17:42:12 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pro Pets</title>
+        <meta name="description" content="You lost we find" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+
+      <footer >
+        
+      </footer>
+    </div>
+  )
+}
+
+export default Home
